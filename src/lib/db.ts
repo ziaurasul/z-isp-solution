b1dd482cd5640c12dab284716f818036bab1f92e
Z User <z@container>
2026-08-03 08:44:52 +0000

    Z ISP Solution v1.0 - Production ready for Vercel deployment

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,8 +6,6 @@ const globalForPrisma = globalThis as unknown as {
 
 export const db =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
-  })
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
